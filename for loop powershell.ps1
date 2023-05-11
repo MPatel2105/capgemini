45205bed9f61b8ba5650fefb6cb0369628a05834
MPatel2105 <132883736+MPatel2105@users.noreply.github.com>
2023-05-11 16:56:23 +0530

    Add files via upload

---
+++ for loop powershell.ps1
@@ -0,0 +1,62 @@
+﻿# For Loop
+
+# 1. Create 10 files
+
+for ($i = 0; $i -lt 10; $i++) {
+    $fileName = "patel$i.txt"
+    $filePath = "C:\Users\MPATEL\powershell\$fileName"
+    New-Item $filePath -ItemType File
+}
+
+
+# 2. Create 10 folders
+for ($i=0;$i -lt 10;$i++)
+{
+New-Item -path "Wahid$i" -ItemType Directory 
+}
+
+
+# 3. Create 10 folders inside the folders created in step 2
+for ($i=0;$i -lt 10;$i++)
+{
+for ($j=0;$j -lt 10;$j++)
+{
+New-Item -path "Wahid$i\folder$j" -ItemType Directory 
+}
+}
+
+
+# 4.Create 10 files in the 10 folders created in step 3 
+for ($i=0;$i -lt 10;$i++)
+{
+for ($j=0;$j -lt 10;$j++)
+{
+for ($k=0;$k -lt 10;$k++)
+{
+New-Item -path "Wahid$i\folder$j\file$k.txt" -ItemType file 
+}
+}
+}
+
+
+
+
+# 5. Copy 10 files to each of 10 folders 
+
+for ($i=0;$i -lt10;$i++){
+for ($j=0;$j -lt10;$j++){
+  $to = 'Wahid' + $i + '\' + 'patel' + $j + '.txt'
+  Copy-item -Path $fro -Destination $to 
+
+}
+}
+
+
+
+
+
+
+# 6. Copy 10 files to each of 10 folders
+
+    Get-ChildItem -path $powershell -Filter *.txt -Recurse | Remove-Item
+
---
+++ while loop.ps1
@@ -0,0 +1,72 @@
+﻿# Create 10 folders
+$i = 1
+while ($i -lt 11){
+    New-Item "C:\Users\MPATEL\powershell\wahid$i" -ItemType Directory
+    $i++
+}
+
+
+
+# Create 10 files
+$i = 1
+while ($i -lt 11){
+    New-item "C:\Users\MPATEL\powershell\file$i" -ItemType File
+    $i++
+}
+
+
+#Create 10 folders inside the folders created in step 2
+
+
+
+$i = 1
+while ($i -lt 11){
+    $j = 1
+    while ($j -lt 11){
+        $abc = 'powershell' + '\Wahid' + $i + '\folder' + $j
+        New-Item -path $abc -ItemType Directory 
+        $j = $j + 1
+        }
+    $i = $i + 1
+}
+
+
+
+#4.Create 10 files in the 10 folders created in step 3 
+
+$i = 1
+while ($i -lt 11){
+    $j = 1
+    while ($j -lt 11){
+        $k = 1
+            while ($k -lt 11){
+                $abc = 'powershell' + '\Wahid' + $i + '\folder' + $j + '\file' + $k + '.txt'
+        New-Item -path $abc -ItemType File
+        $k = $k +1
+        }
+     $j = $j +1
+    }
+  $i = $i + 1
+ } 
+
+
+
+ # 5. Copy 10 files to each of 10 folders
+
+ $i = 1
+while ($i -lt 11){
+    $j = 1
+    while ($j -lt 11){
+        $shift = 'powershell' + '\file' + $j 
+        $to =  'powershell' +'\Wahid' + $i 
+        Copy-Item -Path $shift -Destination $to
+        $j = $j +1
+        }
+     $i = $i + 1
+}
+
+
+
+
+
+
